58b7eaeb015ff9e52c205f603c81c5c6ba32a59d
AlexandrSergienko <alexander.sergienko@livestream.com>
2019-03-12 11:48:16 +0200

    Extend testing objects

--- main.cpp
+++ main.cpp
@@ -10,13 +10,83 @@ class CInsert
 public:
     CInsert()
     {
-        m_int_data = 2;
+        m_int_data = 0;
+        m_int_ptr_data = nullptr;
+        m_int_ptr_data_size = 0;
+        m_std_str_data = "";
+    }
+    CInsert( const CInsert& other )
+    {
+        m_int_data = other.m_int_data;
+        m_int_ptr_data_size = other.m_int_ptr_data_size;
+        m_int_ptr_data = new int [m_int_ptr_data_size];
+        for( size_t count = 0; count < m_int_ptr_data_size; count++ )
+        {
+            m_int_ptr_data[count] = other.m_int_ptr_data[count];
+        }
+        m_std_str_data = other.m_std_str_data;
+    }
+    CInsert( CInsert&& other )
+    {
+        m_int_data = other.m_int_data;
+        m_int_ptr_data_size = other.m_int_ptr_data_size;
+        m_int_ptr_data = other.m_int_ptr_data;
+        m_std_str_data = other.m_std_str_data;
+
+        other.m_int_data = 0;
+        other.m_int_ptr_data_size = 0;
+        delete [] other.m_int_ptr_data;
+        other.m_std_str_data = "";
+    }
+    void operator= ( const CInsert& other )
+    {
+        m_int_data = other.m_int_data;
+        m_int_ptr_data_size = other.m_int_ptr_data_size;
+        m_int_ptr_data = new int [m_int_ptr_data_size];
+        for( size_t count = 0; count < m_int_ptr_data_size; count++ )
+        {
+            m_int_ptr_data[count] = other.m_int_ptr_data[count];
+        }
+        m_std_str_data = other.m_std_str_data;
+    }
+    void operator= ( CInsert&& other )
+    {
+        m_int_data = other.m_int_data;
+        m_int_ptr_data_size = other.m_int_ptr_data_size;
+        m_int_ptr_data = other.m_int_ptr_data;
+        m_std_str_data = other.m_std_str_data;
+
+        other.m_int_data = 0;
+        other.m_int_ptr_data_size = 0;
+        delete [] other.m_int_ptr_data;
+        other.m_std_str_data = "";
     }
     ~CInsert()
     {
-        m_int_ptr_data = new int[2];
-        m_int_ptr_data[0] = 2;
-        m_int_ptr_data[1] = 2;
+        m_int_data = 0;
+        delete [] m_int_ptr_data;
+        m_int_ptr_data = nullptr;
+        m_std_str_data = "";
+    }
+
+    void SetIntData()
+    {
+        m_int_data = 0;
+    }
+
+    void SetIntPtrData()
+    {
+        m_int_ptr_data_size = 5;
+        m_int_ptr_data = new int [m_int_ptr_data_size];
+        for( size_t count = 0; count < m_int_ptr_data_size; count++ )
+        {
+            m_int_ptr_data[count] = static_cast<int> (count);
+        }
+    }
+
+    void SetStdStrData()
+    {
+        m_std_str_data = "Hello world!";
     }
 
     const int GetIntData() const
@@ -27,10 +97,16 @@ public:
     {
         return m_int_ptr_data;
     }
+    const std::string GetStdStrData() const
+    {
+        return m_std_str_data;
+    }
 
 private:
     int m_int_data = 0;
     int* m_int_ptr_data = nullptr;
+    size_t m_int_ptr_data_size = 0;
+    std::string m_std_str_data = "";
 
 };
 // ============================================================
@@ -42,8 +118,7 @@ class CVectorTest
 protected:
     virtual void SetUp() override
     {
-        m_int_vec.assign( 2, 2 );
-        m_class_vec.assign( 2, CInsert() );
+        
     }
 
     virtual void TearDown() override
@@ -52,14 +127,17 @@ protected:
         m_class_vec.clear();
     }
 
+private:
     CVector<int> m_int_vec;
     CVector<CInsert> m_class_vec;
+
 };
 // ============================================================
 
 // ==============================< TESTS >==============================
 TEST_F( CVectorTest, Construct )
 {
+    
     SUCCEED();
 }
 // ============================================================
